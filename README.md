37180fba27e16e8284dc2a8816ea68149f7d216b
1716002875 <1716002875@qq.com>
2023-02-24 22:51:10 +0800

    update version

--- README.md
+++ README.md
@@ -1,95 +1,232 @@
+> 应用场景- 树形结构数据 多列排序
+
+```javascript
+const TreeData = [
+  {
+    base: "base1",
+    BQ: 1,
+    V1: 12,
+    V2: 3,
+    CUS: "LH002",
+    Date: "2022-01-25 18:17:38.861",
+    children: [
+      {
+        base: "base1",
+        BQ: 1,
+        V1: 12,
+        V2: 3,
+        Date: "2050-01-25 18:17:38.861",
+        children: [
+          {
+            base: "base1",
+            BQ: 1,
+            V1: 12,
+            V2: 3,
+            CUS: "LH002",
+            Date: "2022-2-25 18:17:38.861",
+            children: [],
+          },
+          {
+            base: "base1",
+            BQ: 1,
+            V1: 22,
+            V2: 3,
+            CUS: "LH002",
+            Date: "2022-03-25 18:17:38.861",
+            children: [],
+          },
+        ],
+      },
+      {
+        base: "base1",
+        BQ: 1,
+        V1: 22,
+        V2: 3,
+        Date: "2022-07-25 18:17:38.861",
+        children: [],
+      },
+      {
+        base: "base2",
+        BQ: 1,
+        V1: 123,
+        V2: 3,
+        Date: "2022-07-01 18:17:38.861",
+        children: [],
+      },
+    ],
+  },
+  {
+    base: "base1",
+    BQ: 2,
+    V1: 22,
+    V2: 3,
+    CUS: "LH003",
+    Date: "2022-11-25 18:17:38.861",
+    children: [
+      {
+        base: "base1",
+        BQ: 1,
+        V1: 12,
+        V2: 3,
+        CUS: "LH004",
+        Date: "2022-10-15 18:17:38.861",
+        children: [],
+      },
+      {
+        base: "base2",
+        BQ: 1,
+        V1: 22,
+        V2: 3,
+        CUS: "LH005",
+        Date: "2022-10-22 18:17:38.861",
+        children: [],
+      },
+    ],
+  },
+  {
+    base: "base2",
+    BQ: 1,
+    V1: 123,
+    V2: 3,
+    CUS: "LH012",
+    Date: "2022-10-25 18:17:38.861",
+    children: [],
+  },
+  {
+    base: "base2",
+    BQ: 2,
+    V1: 867,
+    V2: 3,
+    CUS: "LH013",
+    Date: "2025-10-25 18:17:38.861",
+    children: [],
+  },
+];
+```
+
 ## 导入
 
 ```javascript
-import XLSort from "xl-sort";
-```
-
-## 实例化
-
-```javascript
+import XLSort from "XLSort";
 const sortInstance = new XLSort();
 ```
 
-## api
+> sub 和 dispatch 时 type 必须一致 如: 'CUTSOM'
+> 排序默认是递归节点下有 children
+
+```typeScript
+  // 排序规则
+    interface SortConfig {
+      configId: Number,
+      order: 'desc' | 'asc',
+      field: String | (Node:Object):String,
+      sortType: 'string' | 'number' | 'date'
+    }
+```
+
+## 简单多列排序
 
 ```javascript
-// 注入 基本conf
-sortInstance.injectBasicConfig({
-  basicConfig: [
-    {
-      configId: 0,
-      order: "asc", // 'asc' 升序 'desc' 降序
-      field: "baseKey", // 每个节点按指定key排序
-      sortType: "string", // 指定排序字段的类型
-    },
-  ],
+// 添加排序规则
+sortInstance.subConf("CUSTOM", {
+  configId: 0, // 主键 用于更新删除排序规则
+  order: "asc", // "asc"升序 或者 "desc"降序
+  field: "base", // 指定以Date字段排序
+  sortType: "string", // 指定字段排序类型 三种 'string' 或 'number' 或者 'date'
+});
+sortInstance.subConf("CUSTOM", {
+  configId: 1,
+  order: "desc",
+  field: "Date",
+  sortType: "date",
+});
+// 排序(不改变原数组)
+sortInstance.dispatchSort("CUSTOM", {
+  data: TreeData,
 });
 ```
 
-```javascript
-// 注入 默认conf
-sortInstance.injectDefaultConfig({
-  type: "BD1001",
-  defaultConfig: [
-    {
-      configId: 2,
-      order: "asc",
-      field: "defaultKey",
-      sortType: "string",
-    },
-  ],
-});
-```
+## 自定义条件多列排序
 
 ```javascript
-// 订阅自定义排序
-sortInstance.subConf({
-  type: "BD1001",
-  config: {
-    configId: 3,
-    order: "desc",
-    // field 或者 where 任选其一 where适合更复杂的场景排序
-    where: (Node) => {
-      // 如果节点(node)的 type属性为 1 ,则按节点的Au属性排序
-      if (Node.type === 1) {
-        return "Au";
-      }
-      // 如果节点(node)的 type属性为 2 ,则按节点的Bu属性排序
-      if (Node.type === 2) {
-        return "Bu";
-      }
-    },
-    sortType: "number",
+// 添加排序规则
+sortInstance.subConf("CUSTOM2", {
+  configId: 0, // 主键用于 更新 删除排序规则
+  order: "asc", // "asc"升序 或者 "desc"降序
+  field: "base", // 指定以Date字段排序 类型为 string || function
+  sortType: "string", // 指定字段排序类型 三种 'string' 或 'number' 或者 'date'
+});
+sortInstance.subConf("CUSTOM2", {
+  configId: 1,
+  order: "desc",
+  sortType: "date",
+  // 自定义字段排序
+  field: (Node) => {
+    if (Node.BQ === 1) {
+      // 当前节点BQ的值为1 则取节点 V1 的值
+      return "V1";
+    }
+    if (Node.BQ === 2) {
+      // 当前节点BQ的值为2 则取节点 V2 的值
+      return "V2";
+    }
   },
 });
-```
-
-```javascript
-// 删除 排序方法
-sortInstance.deleteSubConf({
-  type: "BD1001",
-  configId: 3,
+sortInstance.dispatchSort("CUSTOM2", {
+  data: TreeData,
 });
 ```
 
+## 删除规则
+
 ```javascript
-// 更新 排序方法
-sortInstance.updateSubConf({
-  type: "BD1001",
-  configId: 3,
-  config: {
-    order: "desc",
-  },
+// 删除 sub添加的 configId为 2 的规则
+sortInstance.deleteConf("CUSTOM", 2);
+```
+
+## 更新规则
+
+```javascript
+// 更新 sub添加的 configId为 0 的规则
+sortInstance.updateConf("CUSTOM", 0, {
+  configId: 2,
+  order: "desc", // 更新order为'desc'
+  // ...
 });
 ```
 
 ## 排序
 
 ```javascript
-// 排序
-sortInstance.dispatchSort({
-  type: "BD1001",
-  data: tree, // tree是一个树结构的数据 或者数组
-  childrenFieldName: "child", // 选填 如果是树结构数据 children属性重命名
+sortInstance.dispatchSort("CUSTOM", {
+  data: TreeData,
+  childFieldAlias: "child", // 可选 递归时子节点别名设置 children => child
+});
+```
+
+## 其他
+
+```javascript
+// 添加基底排序
+// CUSTOM上的排序规则 都会以在基底排序之上 再排序
+sortInstance.subBaseConf("CUSTOM", {
+  configId: 0,
+  order: "desc",
+  field: "base",
+  sortType: "string",
+});
+// 添加默认排序 如果CUSTOM规则中 已经subConf新加的自定义排序 那么默认排序将会失效 所以最终的排序规则为 最终排序的规则 BaseConf + DefaultConf || Conf
+sortInstance.subDefaultConf("CUSTOM", {
+  configId: 2,
+  order: "desc",
+  sortType: "number",
+  field: (Node) => {
+    if (Node.BQ === 1) {
+      return "V2";
+    }
+    if (Node.BQ === 2) {
+      return "V1";
+    }
+  },
 });
 ```
--- index.js
+++ index.js
@@ -1,47 +1,39 @@
-/**
- * sort-config
- * {
- *  configId: Number|String
- *  order: 'asc'|'desc'
- *  field:  String,
- *  sortType: 'string' | 'number' | 'date'
- *  where: (param:Object):String => {}
- * }
- */
 class XLSort {
     constructor() {
-        this.defaultConfig = [];
-        this.basicConfig = [];
+        this.configIdList = []
+        this.defaultConfig = {};
+        this.baseConfig = {};
         this.config = {};
-        this.childrenFieldName = "";
+        this.childFieldAlias = '';
     }
-    isRightParam(paramList) {
+    warn() {
+        console.warn(`Function parameter format warn
+      {
+          configId: Number | String
+          order: 'asc' | 'desc'
+          field: String
+          sortType: 'string' | 'number' | 'date'
+          where: (param: Object): String
+      }`);
+    }
+    isVerificationPassed(param) {
         let result = true;
-        paramList.map((param) => {
-            const isRightId = param.id && typeof param.id === "number";
-            const isRightOrder =
-                param.order && (param.order === "asc" || param.order === "desc");
-            const isRightField = param.field && this.isString(param.field);
-            const isSortType =
-                param.sortType &&
-                (param.sortType === "string" ||
-                    param.sortType === "number" ||
-                    param.sortType === "date");
-            const isHaveWhereFunc = typeof param.where === "function";
-            isRightId && isRightOrder && isSortType ?
-                (result = true) :
-                (result = false);
-            isRightField || isHaveWhereFunc ? (result = true) : (result = false);
-        });
+        const isRightId = typeof param.configId === 'number';
+        const isRightOrder = param.order === 'asc' || param.order === 'desc';
+        const isRightFieldType = this.isString(param.field) || typeof param.field === 'function'
+        const isRightField = param.field && isRightFieldType
+        const isSortType =
+            (param.sortType === 'string' ||
+                param.sortType === 'number' ||
+                param.sortType === 'date');
+        isRightId && isRightOrder && isSortType && isRightField ?
+            (result = true) :
+            (result = false);
+        if (this.configIdList.includes(param.configId) || !Reflect.has(param, 'configId')) {
+            result = false
+        }
         if (!result) {
-            console.error(`Function parameter format error 
-            {
-                configId: Number | String
-                order: 'asc' | 'desc'
-                field: String
-                sortType: 'string' | 'number' | 'date'
-                where: (param: Object): String => { }
-            }`);
+            this.warn()
         }
         return result;
     }
@@ -49,83 +41,56 @@ class XLSort {
         return Array.isArray(n);
     }
     isString(n) {
-        return typeof n === "string";
+        return typeof n === 'string';
     }
     isObject(n) {
-        return n.__proto__ === Object.prototype;
+        return Object.prototype.toString.call(n) === '[object Object]';
     }
-    // 注入 基本排序Conf
-    /**
-     *
-     * @param {[]} basicConfig
-     */
-    injectBasicConfig({
-        basicConfig = []
-    }) {
-        if (!this.isArray(basicConfig)) {
-            console.error("Function basicConfig parameter type error must be Array");
-            return;
-        }
-        if (!this.isRightParam(basicConfig)) {
-            return;
-        }
-        this.basicConfig = basicConfig;
-        return this;
-    }
-    // 注入 默认排序Conf
-    /**
-     *
-     * @param {String} type
-     * @param {[]} defaultConfig
-     */
-    injectDefaultConfig({
-        type,
-        defaultConfig = []
-    }) {
+    subBaseConf(type, baseConfig) {
         if (!this.isString(type)) {
-            console.error("Function type parameter type error must be String");
+            return
+        }
+        if (!this.isVerificationPassed(baseConfig)) {
             return;
         }
-        if (!this.isArray(defaultConfig)) {
-            console.error(
-                "Function defaultConfig parameter type error must be Array"
-            );
-            return;
+        if (type in this.baseConfig) {
+            this.baseConfig[type].push(baseConfig)
+        } else {
+            this.baseConfig[type] = [baseConfig]
         }
-        if (!this.isRightParam(defaultConfig)) {
-            return;
-        }
-        this.defaultConfig[type] = defaultConfig;
         return this;
     }
-    // 清楚所有规则
-    clearAllConfig() {
-        this.defaultConfig = [];
-        this.basicConfig = [];
-        this.config = {};
+    subDefaultConf(type, config) {
+        if (!this.isString(type)) {
+            return;
+        }
+        if (!this.isVerificationPassed(config)) {
+            return;
+        }
+        if (type in this.defaultConfig) {
+            this.defaultConfig[type].push(config)
+        } else {
+            this.defaultConfig[type] = [config]
+        }
+
+        return this;
     }
-    // 合并排序规则
     mergeUseConfig(type) {
-        // 基本排序规则
-        const basicConfig = this.basicConfig;
-        // 默认排序
-        const defaultConfig = this.defaultConfig[type];
-        // 新加排序
-        const customConfig = this.config[type];
-        // 最终排序规则
+        const baseConfig = this.baseConfig[type] || []
+        const defaultConfig = this.defaultConfig[type] || []
+        const customConfig = this.config[type] || []
         let configList = [];
         if (customConfig && customConfig.length > 0) {
-            configList = basicConfig.concat(customConfig);
+            configList = baseConfig.concat(customConfig);
         } else {
             if (defaultConfig && defaultConfig.length > 0) {
-                configList = basicConfig.concat(defaultConfig);
+                configList = baseConfig.concat(defaultConfig);
             } else {
-                configList = basicConfig;
+                configList = baseConfig;
             }
         }
         return configList;
     }
-    // 转换value
     transformVal(cur, next, config) {
         // val
         let curVal = null;
@@ -133,52 +98,49 @@ class XLSort {
         // field
         let curField = null;
         let nextField = null;
-        const field = config.field;
-        const whereFunc = config.where;
-        if (field) {
-            curField = field;
-            nextField = field;
-        } else if (whereFunc) {
-            curField = whereFunc(cur);
-            nextField = whereFunc(next);
+        if (typeof config.field === 'string') {
+            curField = config.field;
+            nextField = config.field;
+        } else if (typeof config.field === 'function') {
+            curField = config.field(cur);
+            nextField = config.field(next);
         }
         if (!curField || !nextField) {
-            console.error("transformVal params error");
             return {
-                curVal: "",
-                nextVal: "",
+                curVal: '',
+                nextVal: ''
             };
         }
         switch (config.sortType) {
             // 字符串
-            case "string":
-                curField in cur ? (curVal = cur[curField].toString()) : (curVal = "");
+            case 'string':
+                curField in cur ? (curVal = cur[curField].toString()) : (curVal = '');
                 nextField in next ?
                     (nextVal = next[nextField].toString()) :
-                    (nextVal = "");
+                    (nextVal = '');
                 break;
-            case "number":
+            case 'number':
                 curField in cur ? (curVal = Number(cur[curField])) : (curVal = 0);
                 nextField in next ? (nextVal = Number(next[nextField])) : (nextVal = 0);
                 break;
-            case "date":
+            case 'date':
                 curField in cur ? (curVal = Date.parse(cur[curField])) : (curVal = 0);
                 nextField in next ?
                     (nextVal = Date.parse(next[nextField])) :
                     (nextVal = 0);
                 break;
             default:
-                curVal = "";
-                nextVal = "";
+                curVal = '';
+                nextVal = '';
         }
         return {
             curVal,
-            nextVal,
+            nextVal
         };
     }
     handleSort(curVal, nextVal, config) {
-        if (config.sortType === "string") {
-            if (config.order === "asc") {
+        if (config.sortType === 'string') {
+            if (config.order === 'asc') {
                 const ascD = curVal.localeCompare(nextVal);
                 return ascD;
             } else {
@@ -186,34 +148,21 @@ class XLSort {
                 return descD;
             }
         } else {
-            if (config.order === "asc") {
+            if (config.order === 'asc') {
                 return curVal - nextVal;
             } else {
                 return nextVal - curVal;
             }
         }
     }
-    getConf({
-        type
-    }) {
+    subConf(type, config) {
         if (!this.isString(type)) {
-            console.error("Function type parameter type error must be String");
-        }
-        return this.config[type];
-    }
-    subConf({
-        type,
-        config
-    }) {
-        if (!this.isString(type)) {
-            console.error("Function type parameter type error must be String");
             return;
         }
         if (!this.isObject(config)) {
-            console.error("Function config  parameter type error must be Object");
             return;
         }
-        if (!this.isRightParam([config])) {
+        if (!this.isVerificationPassed(config)) {
             return;
         }
         if (type in this.config) {
@@ -222,58 +171,92 @@ class XLSort {
             this.config[type] = [config];
         }
     }
-    deleteSubConf({
-        type,
-        configId
-    }) {
+    deleteConf(type, configId) {
         if (!this.isString(type)) {
-            console.error("Function type parameter type error must be String");
             return;
         }
-        if (!this.isString(configId) && typeof configId !== "number") {
-            console.error(
-                "Function configId type parameter type error must be String"
-            );
-            return;
+        let isDeleted = false
+        const config = this.config[type] || []
+        for (let i = config.length - 1; i >= 0; i--) {
+            if (config[i].configId === configId) {
+                isDeleted = true
+                config.splice(i, 1);
+                break
+            }
         }
-        const configList = this.config[type];
-        for (let i = configList.length - 1; i >= 0; i--) {
-            if (configList[i].configId === configId) {
-                configList.splice(i, 1);
+        if (isDeleted) {
+            return
+        }
+        const defaultConfig = this.defaultConfig[type] || []
+        for (let i = defaultConfig.length - 1; i >= 0; i--) {
+            if (defaultConfig[i].configId === configId) {
+                isDeleted = true
+                defaultConfig.splice(i, 1);
+                break
+            }
+        }
+        if (isDeleted) {
+            return
+        }
+        const baseConfig = this.baseConfig[type] || []
+        for (let i = baseConfig.length - 1; i >= 0; i--) {
+            if (baseConfig[i].configId === configId) {
+                baseConfig.splice(i, 1);
             }
         }
     }
-    updateSubConf({
-        type,
-        configId,
-        config
-    }) {
+    updateConf(type, configId, updateConfig) {
         if (!this.isString(type)) {
-            console.error("Function type parameter type error must be String");
             return;
         }
-        if (!this.isString(configId) && typeof configId !== "number") {
-            console.error("Function configId parameter type error must be String");
-            return;
+        let isDeleted = false
+        if (this.isArray(this.config[type])) {
+            for (let i = this.config[type].length - 1; i >= 0; i--) {
+                if (this.config[type][i].configId === configId) {
+                    isDeleted = true
+                    this.config[type][i] = {
+                        ...this.config[type][i],
+                        ...updateConfig
+                    }
+                    break
+                }
+            }
         }
-        if (!this.isObject(config)) {
-            console.error("Function config parameter type error must be Object");
-            return;
+        if (isDeleted) {
+            return
         }
-        const configList = this.config[type];
-        for (let i = configList.length - 1; i >= 0; i--) {
-            if (configList[i].configId === configId) {
-                configList[i] = {
-                    ...configList[i],
-                    ...config,
-                };
+        if (this.isArray(this.defaultConfig[type])) {
+            for (let i = this.defaultConfig[type].length - 1; i >= 0; i--) {
+                if (this.defaultConfig[type][i].configId === configId) {
+                    isDeleted = true
+
+                    this.defaultConfig[type][i] = {
+                        ...this.defaultConfig[type][i],
+                        ...updateConfig
+                    }
+                    break
+                }
+            }
+        }
+        if (isDeleted) {
+            return
+        }
+        if (this.isArray(this.baseConfig[type])) {
+            for (let i = this.baseConfig[type].length - 1; i >= 0; i--) {
+                if (this.baseConfig[type][i].configId === configId) {
+                    this.baseConfig[type][i] = {
+                        ...this.baseConfig[type][i],
+                        ...updateConfig
+                    }
+                    break
+                }
             }
         }
     }
     sortChild(data, configList) {
         for (let i = 0; i < data.length; i++) {
-            if (data[i][this.childrenFieldName]) {
-                this.main(configList, data[i][this.childrenFieldName]);
+            if (data[i][this.childFieldAlias]) {
+                this.main(configList, data[i][this.childFieldAlias]);
             }
         }
     }
@@ -303,32 +286,25 @@ class XLSort {
         });
         return this.sortChild(data, configList);
     }
-    dispatchSort({
-        type,
+    dispatchSort(type, {
         data,
-        childrenFieldName
+        childFieldAlias
     }) {
-        this.childrenFieldName = childrenFieldName || "children";
+        this.childFieldAlias = childFieldAlias || 'children';
         if (!this.isString(type)) {
-            console.error("Function type parameter type error must be String");
             return;
         }
-        if (!this.isString(this.childrenFieldName)) {
-            console.error(
-                "Function childrenFieldName parameter type error must be String"
-            );
+        if (!this.isString(this.childFieldAlias)) {
             return;
         }
         if (!this.isArray(data)) {
-            console.error("Function data parameter type error must be Array");
             return;
         }
         const configList = this.mergeUseConfig(type);
-        console.log("configList", configList);
         this.main(configList, data);
         return {
-            data,
+            data
         };
     }
 }
-export default XLSort
+export default new XLSort()
